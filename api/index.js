a3a45c3aeb35130786648d695d742bfe4a1549b5
therongraphictexte <therongraphicsystem2025@gmail.com>
2025-07-14 21:08:16 +0100

    Delete api directory
    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// api/index.js
-
-export default async function handler(req, res) {
-  res.status(200).json({
-    message: 'Backend API do Theron está ativo!',
-    endpointsDisponiveis: ['/api/impressao']
-  });
-}
